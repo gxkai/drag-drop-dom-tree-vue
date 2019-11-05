56d94be436955bdfc61627ff1744fd84b2132545
guxukai <1378026744@qq.com>
2019-11-05 11:33:30 +0800

    111

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-const port = process.env.PORT || 80
+const port = process.env.PORT || 8082
 
 app = express()
 app.use(serveStatic('public', {'index': ['frame.html']}))
